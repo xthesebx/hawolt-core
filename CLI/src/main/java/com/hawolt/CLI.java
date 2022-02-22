8a793d2cc27f19db0b59c699f25fd3e457371df5
hawolt <hawolton@gmail.com>
2022-02-22 16:07:09 +0100

    fixed problem with Argument hashCode

--- CLI/src/main/java/com/hawolt/CLI.java
+++ CLI/src/main/java/com/hawolt/CLI.java
@@ -4,26 +4,28 @@ import java.util.*;
 
 public class CLI extends HashSet<Argument> {
 
-    private final Map<Argument, List<Argument>> INSTRUCTIONS = new HashMap<>();
+    private final Map<Argument, List<String>> INSTRUCTIONS = new HashMap<>();
 
     public void put(Argument arg) throws ParserException {
-        for (Argument argument : INSTRUCTIONS.values().stream().flatMap(Collection::stream).toArray(Argument[]::new)) {
+        for (Argument argument : INSTRUCTIONS.keySet()) {
             if (arg.isUnique() && (argument.getShortName().equals(arg.getShortName()) || argument.getLongName().equals(arg.getLongName()))) {
                 throw new ParserException(String.format("Unique argument [%s|%s] already present", arg.getShortName(), arg.getLongName()));
             }
         }
         if (!INSTRUCTIONS.containsKey(arg)) INSTRUCTIONS.put(arg, new ArrayList<>());
-        INSTRUCTIONS.get(arg).add(arg);
+        INSTRUCTIONS.get(arg).add(arg.getValue());
     }
 
     public boolean has(String key) {
-        for (Argument argument : INSTRUCTIONS.values().stream().flatMap(Collection::stream).toArray(Argument[]::new)) {
-            if (argument.getLongName().equals(key) || argument.getShortName().equals(key)) return true;
+        for (Argument argument : INSTRUCTIONS.keySet()) {
+            if (argument.getLongName().equals(key) || argument.getShortName().equals(key)) {
+                return INSTRUCTIONS.containsKey(argument);
+            }
         }
         return false;
     }
 
-    public List<Argument> get(String key) {
+    public List<String> get(String key) {
         for (Argument argument : INSTRUCTIONS.keySet()) {
             if (argument.getLongName().equals(key) || argument.getShortName().equals(key)) {
                 return INSTRUCTIONS.get(argument);
@@ -35,7 +37,7 @@ public class CLI extends HashSet<Argument> {
     public String getValue(String key) {
         for (Argument argument : INSTRUCTIONS.keySet()) {
             if (argument.getLongName().equals(key) || argument.getShortName().equals(key)) {
-                return INSTRUCTIONS.get(argument).get(0).getValue();
+                return INSTRUCTIONS.get(argument).get(0);
             }
         }
         return null;
