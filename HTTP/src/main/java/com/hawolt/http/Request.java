ee177308882bd8801b752482c94ff66408ca308c
hawolt <hawolton@gmail.com>
2022-07-30 16:41:53 +0200

    added getters to fetch request values

--- HTTP/src/main/java/com/hawolt/http/Request.java
+++ HTTP/src/main/java/com/hawolt/http/Request.java
@@ -4,27 +4,40 @@ package com.hawolt.http;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.ProtocolException;
 import java.net.Proxy;
 
 public class Request {
-    private final HttpURLConnection connection;
+    private HttpURLConnection connection;
+    private final String endpoint;
+    private final boolean output;
+    private final Method method;
+    private final Proxy proxy;
 
-    public Request(String endpoint) throws IOException {
+    public Request(String endpoint) {
         this(endpoint, null);
     }
 
-    public Request(String endpoint, Proxy proxy) throws IOException {
+    public Request(String endpoint, Proxy proxy) {
         this(endpoint, proxy, Method.GET, false);
     }
 
-    public Request(String endpoint, Method method, boolean output) throws IOException {
+    public Request(String endpoint, Method method, boolean output) {
         this(endpoint, null, method, output);
     }
 
-    public Request(String endpoint, Proxy proxy, Method method, boolean output) throws IOException {
+    public Request(String endpoint, Proxy proxy, Method method, boolean output) {
+        this.endpoint = endpoint;
+        this.method = method;
+        this.output = output;
+        this.proxy = proxy;
+    }
+
+    private Request prepare() throws IOException {
         connection = BasicHttp.open(endpoint, proxy);
         connection.setRequestMethod(method.name());
         connection.setDoOutput(output);
+        return this;
     }
 
     public void addHeader(String key, String value) {
@@ -38,12 +51,28 @@ public class Request {
         }
     }
 
+    public String getEndpoint() {
+        return endpoint;
+    }
+
+    public boolean isOutput() {
+        return output;
+    }
+
+    public Method getMethod() {
+        return method;
+    }
+
+    public Proxy getProxy() {
+        return proxy;
+    }
+
     public HttpURLConnection getConnection() {
         return connection;
     }
 
 
     public Response execute() throws IOException {
-        return new Response(this);
+        return new Response(prepare());
     }
 }
