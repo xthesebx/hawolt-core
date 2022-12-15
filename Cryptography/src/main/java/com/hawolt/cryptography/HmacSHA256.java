34f5b95bb0973aa3da557a6e19f46e45f800c21f
hawolt <hawolton@gmail.com>
2022-12-15 16:35:03 +0100

    added hmacsha256

---
+++ Cryptography/src/main/java/com/hawolt/cryptography/HmacSHA256.java
@@ -0,0 +1,36 @@
+package com.hawolt.cryptography;
+
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created: 06/12/2022 18:04
+ * Author: Twitter @hawolt
+ **/
+
+public class HmacSHA256 {
+
+    public static String generate(String message, String key) throws InvalidKeyException, NoSuchAlgorithmException {
+        byte[] bytes = hmac(key.getBytes(), message.getBytes());
+        return bytesToHex(bytes);
+    }
+
+    private static byte[] hmac(byte[] key, byte[] message) throws NoSuchAlgorithmException, InvalidKeyException {
+        Mac mac = Mac.getInstance("HmacSHA256");
+        mac.init(new SecretKeySpec(key, "HmacSHA256"));
+        return mac.doFinal(message);
+    }
+
+    private static String bytesToHex(byte[] bytes) {
+        final char[] hexArray = "0123456789abcdef".toCharArray();
+        char[] hexChars = new char[bytes.length * 2];
+        for (int j = 0, v; j < bytes.length; j++) {
+            v = bytes[j] & 0xFF;
+            hexChars[j * 2] = hexArray[v >>> 4];
+            hexChars[j * 2 + 1] = hexArray[v & 0x0F];
+        }
+        return new String(hexChars);
+    }
+}
