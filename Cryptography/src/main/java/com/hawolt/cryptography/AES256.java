2afca6c8b4024d86defd324e1fb2d9732a9a556b
xXTheSebXx <sebastianjerke@gmail.com>
2024-10-31 22:43:53 +0100

    update dependencies

--- Cryptography/src/main/java/com/hawolt/cryptography/AES256.java
+++ Cryptography/src/main/java/com/hawolt/cryptography/AES256.java
@@ -14,36 +14,32 @@ import java.util.Base64;
 
 public class AES256 {
     private final String secret, salt;
+    private final byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+    private final IvParameterSpec ivspec = new IvParameterSpec(iv);
+    KeySpec spec;
+    SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+    SecretKeySpec secretKey;
+    Cipher cipher;
 
-    private AES256(String secret, String salt) {
+    private AES256(String secret, String salt) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException {
         this.secret = secret;
         this.salt = salt;
+        spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
+        SecretKey tmp = factory.generateSecret(spec);
+        secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
+        cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
     }
 
-    public static AES256 init(String secret, String salt) {
+    public static AES256 init(String secret, String salt) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeySpecException {
         return new AES256(secret, salt);
     }
 
-    public String encrypt(String s) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, InvalidAlgorithmParameterException, InvalidKeyException, UnsupportedEncodingException, BadPaddingException, IllegalBlockSizeException {
-        byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        IvParameterSpec ivspec = new IvParameterSpec(iv);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
-        SecretKey tmp = factory.generateSecret(spec);
-        SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+    public String encrypt(String s) throws InvalidAlgorithmParameterException, InvalidKeyException, UnsupportedEncodingException, BadPaddingException, IllegalBlockSizeException {
         cipher.init(Cipher.ENCRYPT_MODE, secretKey, ivspec);
         return Base64.getEncoder().encodeToString(cipher.doFinal(s.getBytes("UTF-8")));
     }
 
-    public String decrypt(String s) throws NoSuchAlgorithmException, InvalidKeySpecException, NoSuchPaddingException, BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
-        byte[] iv = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-        IvParameterSpec ivspec = new IvParameterSpec(iv);
-        SecretKeyFactory factory = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        KeySpec spec = new PBEKeySpec(secret.toCharArray(), salt.getBytes(), 65536, 256);
-        SecretKey tmp = factory.generateSecret(spec);
-        SecretKeySpec secretKey = new SecretKeySpec(tmp.getEncoded(), "AES");
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+    public String decrypt(String s) throws BadPaddingException, IllegalBlockSizeException, InvalidAlgorithmParameterException, InvalidKeyException {
         cipher.init(Cipher.DECRYPT_MODE, secretKey, ivspec);
         return new String(cipher.doFinal(Base64.getDecoder().decode(s)));
     }
